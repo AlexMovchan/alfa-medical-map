fbffd8e4c3885bdba6e4dd722f6b03e20673e3b1
AlexMovchan <olexandr.movchan@gmail.com>
2019-08-17 15:47:54 +0200

    added category to map

--- src/CustomMarker.js
+++ src/CustomMarker.js
@@ -5,6 +5,7 @@ import { Marker, Popup } from 'react-leaflet';
 const CustomMarker = ({ marker }) => 
   <Marker position={[Number(marker.lat), Number(marker.lon)]}>
     <Popup>
+      <h4>Категорія {marker.category}</h4>
       <h4>{marker.specialization} {marker.title}</h4>
       {marker.address}
     </Popup>
